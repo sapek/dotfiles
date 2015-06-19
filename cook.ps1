f697888da813791587273009e5eb708752e1c81b
Adam Sapek <adamsap@microsoft.com>
2015-06-19 10:59:55 -0700

    Remove cloning of dotfiles repo from cook.ps1

--- cook.ps1
+++ cook.ps1
@@ -1,11 +1,2 @@
-# "Clone" the dotfiles repo into home directory
-cd $HOME
-git init
-git remote add origin https://github.com/sapek/dotfiles.git
-git fetch
-git checkout --track origin/master
-git submodule init
-git submodule update
-
 # Apply the Chef recipe bootstrap
 chef-client --local-mode --runlist 'recipe[bootstrap]'
